54e0f37ac4bd5141f317a560227cdb9c7fd26faa
dbalanda <dante.balanda@gmail.com>
2022-01-16 15:15:29 -0300

    añadido tooltip en botón de formulario, por si acaso.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,9 @@
 
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
 $(document).scroll( function(e){
 const y = $("html").scrollTop();
 if(y > 300) $("nav").addClass("fondonav")
